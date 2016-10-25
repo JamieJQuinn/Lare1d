942bccf06d7871a07460767caa88bb026273ab4a
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2016-10-25 11:50:25 +0100

    First cpp file. Does nothing.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+typedef double real;
+
+class Variables {
+  real *density, *pressure, *velocity;
+};
+
+int saveState() {}
+
+int main(int argc, char** argv) {
+  
+}
