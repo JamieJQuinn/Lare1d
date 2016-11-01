7d6b6c53658eaf2ab4d35b5bc638a59927fa3f84
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2016-11-01 12:23:05 +0000

    Added flux limiter calculator

---
+++ FluxLimiter.cpp
@@ -0,0 +1,25 @@
+#include "FluxLimiter.hpp"
+#include <cmath>
+#include <algorithm>
+
+using std::abs;
+using std::min;
+
+real FluxLimiter::calcAbsLimiterAt(int i, real* phi, real* f, real* u, real* dxc, real* dxb) {
+  real absD = (2-phi[i])/3*abs(f[i+1] - f[i])/dxc[i];
+  if(u[i]>0) {
+    absD += (1+phi[i])/3*abs(f[i] - f[i-1])/dxc[i-1];
+  } else {
+    absD += (1+phi[i])/3*abs(f[i+2] - f[i+1])/dxc[i-1];
+  }
+
+  return calcSignAt(i, f)*min(absD*dxb[i], min(2*abs(f[i+1] - f[i]), 2*abs(f[i] - f[i-1])));
+}
+
+int FluxLimiter::calcSignAt(int i, real* f) {
+  if(sign(f[i+1] - f[i]) == sign(f[i] - f[i-1])) {
+    return sign(f[i+1] - f[i]);
+  } else {
+    return 0;
+  }
+}
---
+++ FluxLimiter.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "Precision.hpp"
+
+class FluxLimiter {
+  public:
+  real calcAbsLimiterAt(int i, real* phi, real* f, real* u, real* dxc, real* dxb);
+
+  private:
+  int sign(real x) {
+    return (x > 0) - (x < 0);
+  }
+  int calcSignAt(int i, real* f);
+};
