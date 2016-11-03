e70c2df04c20f733558221593b76e93e8a5b7b23
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2016-11-03 11:25:43 +0000

    Cleanup test

--- test/ModelVariablesTest.cpp
+++ test/ModelVariablesTest.cpp
@@ -1,21 +1,29 @@
 #include "catch.hpp"
 #include <ModelVariables.hpp>
+#include <Constants.hpp>
+#include <iostream>
 
 TEST_CASE( "ModelVariables save and load correctly", "[variables]" ) {
-  ModelVariables vars(10);
+  int N = 10;
+  std::string filePath = "ModelVariableSaveTest.dat";
+  std::remove(filePath.c_str());
+  const Constants c(0.0001f, 2.0f, N, 10, 2.0f, 3.0f);
+  ModelVariables vars(N);
 
   real* data = vars.pressure.get();
   for(int i=0; i<5; ++i) {
     data[i] = 4.0f;
   }
 
-  vars.save("VariableSaveTest.dat");
+  vars.save(filePath.c_str());
 
-  ModelVariables vars2(10);
+  ModelVariables vars2(N);
 
-  vars2.load("VariableSaveTest.dat");
+  vars2.load(filePath.c_str(), c);
 
   for(int i=0; i<vars.len(); ++i) {
-    REQUIRE(vars.pressure[i] == vars2.pressure[i]);
+    CHECK(vars.pressure[i] == vars2.pressure[i]);
+    CHECK(vars.density[i] == vars2.density[i]);
+    CHECK(vars.velocity[i] == vars2.velocity[i]);
   }
 }
