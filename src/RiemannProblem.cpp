c7da358b2fdc931317ecdfaa3190f22b0400056e
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2016-11-03 12:14:47 +0000

    Calculate energy initially

--- src/RiemannProblem.cpp
+++ src/RiemannProblem.cpp
@@ -8,6 +8,7 @@ void setupInitialConditions(ModelVariables &vars, const Constants &c) {
   real *pressure = vars.pressure.get();
   real *density = vars.density.get();
   real *velocity = vars.velocity.get();
+  real *energy = vars.energy.get();
   for(int i = 0; i < c.nGridPoints*0.5f; ++i) {
     pressure[i] = 128.0f;
     density[i] = 256.0f/49.0f;
@@ -17,6 +18,10 @@ void setupInitialConditions(ModelVariables &vars, const Constants &c) {
     pressure[i] = 1.0f;
     density[i] = 2.0f/25.0f;
     velocity[i] = 0.0f;
+  }
+
+  for(int i=0; i<vars.len(); ++i) {
+    energy[i] = pressure[i]/(density[i]*(c.gamma-1));
   }
 }
 
