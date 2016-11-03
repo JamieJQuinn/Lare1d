0f68ff66f07d428c6083158762133dd489354e74
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2016-11-03 11:23:13 +0000

    Added max time steps to constants

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -7,8 +7,9 @@ class Constants {
     real dt;
     real gamma;
     int nGridPoints;
+    int nTimeSteps;
     real sigma1;
     real sigma2;
 
-    Constants(real dtIn, real gammaIn, int nGridPointsIn, real sigma1In, real sigma2In);
+    Constants(real dtIn, real gammaIn, int nGridPointsIn, int nTimeStepsIn, real sigma1In, real sigma2In);
 };
--- src/Constants.cpp
+++ src/Constants.cpp
@@ -1,9 +1,10 @@
 #include <Constants.hpp>
 
-Constants::Constants(real dtIn, real gammaIn, int nGridPointsIn, real sigma1In, real sigma2In):
+Constants::Constants(real dtIn, real gammaIn, int nGridPointsIn, int nTimeStepsIn, real sigma1In, real sigma2In):
   dt(dtIn),
   gamma(gammaIn),
   nGridPoints(nGridPointsIn),
+  nTimeSteps(nTimeStepsIn),
   sigma1(sigma1In),
   sigma2(sigma2In)
 {}
