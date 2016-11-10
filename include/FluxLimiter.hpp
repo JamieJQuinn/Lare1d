8a83365611676789e21114d69c57f76cf6b6533d
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2016-11-10 11:44:31 +0000

    Improved flux limiter

--- include/FluxLimiter.hpp
+++ include/FluxLimiter.hpp
@@ -5,5 +5,4 @@ class FluxLimiter {
   public:
   static real calcAt(int i, real phi, real* f, real uBar, real* dxc, real* dxb);
   static int sign(real x);
-  static int calcSignAt(int i, real* f);
 };
--- src/FluxLimiter.cpp
+++ src/FluxLimiter.cpp
@@ -11,20 +11,13 @@ real FluxLimiter::calcAt(int i, real phi, real* f, real uBar, real* dxc, real* d
   if(uBar>0.0f) {
     absD += (1.0f+phi)/3.0f*abs(f[i] - f[i-1])/dxc[i-1];
   } else {
-    absD += (1.0f+phi)/3.0f*abs(f[i+2] - f[i+1])/dxc[i-1];
+    absD += (1.0f+phi)/3.0f*abs(f[i+2] - f[i+1])/dxc[i+1];
   }
-  real mindf = 2*min(abs(f[i+1] - f[i]), abs(f[i] - f[i-1]));
-  real s = real(calcSignAt(i, f));
+  real dfMin = 2*min(abs(f[i+1] - f[i]), abs(f[i] - f[i-1]));
+  real s = 0.5f*(sign(f[i+1] - f[i]) + sign(f[i] - f[i-1]));
 
-  return s*min(200*absD*dxb[i], mindf);
-}
-
-int FluxLimiter::calcSignAt(int i, real* f) {
-  if(sign(f[i+1] - f[i]) == sign(f[i] - f[i-1])) {
-    return sign(f[i+1] - f[i]);
-  } else {
-    return 0;
-  }
+  //return s*absD;
+  return s*min(absD*dxb[i], dfMin);
 }
 
 int FluxLimiter::sign(real x) {
