d96e8507d084586ce498905c8315623d5c774d23
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2016-11-23 17:39:04 +0000

    Removed example running tools

--- tools/run_riemann.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd data/RiemannData
-rm *.dat
-../../lare1d
-../../analysis/plot.py -c show -N 800 --over=analyticalSoln.dat  RiemannSoln.dat
-cd ../..
--- tools/run_sod.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p data/SodData
-cd data/SodData
-rm *.dat
-../../lare1d
-../../analysis/plot.py -c show -N 500 SodSoln.dat
-cd ../..
